ad44024b84e864e1bf3b25633447792db5c51e43
Chris McCormick <chris@mccormick.cx>
2015-11-22 21:14:52 +0800

    Demo producing bonk sounds made of enveloped sinewaves.

--- src/sir_mix_a_loop/dev.cljs
+++ src/sir_mix_a_loop/dev.cljs
@@ -7,7 +7,11 @@
 
 ; (defonce app-state (atom {:text "Hello world!"}))
 
-(def my-loop {:length-in-ms 1000})
+(defonce samples (atom []))
+
+(def ticks 16)
+
+(def my-loop {:length-in-ms 800})
 
 (defonce looper
   (if mix-loop/audio?
@@ -20,12 +24,44 @@
   (print "Mixing.")
   (mix-loop/update-loop looper my-loop))
 
+(defn samples-watcher
+  [watcher-key atom-changed old-state new-state]
+  (mix-loop/update-loop looper (assoc my-loop :samples @samples)))
+
+; when the samples atom is changed, update the loop
+(defonce watcher
+  (add-watch samples :loop-updater samples-watcher))
+
 ; create our user interface
 (set! (.-innerHTML (js/document.getElementById "app"))
-      (apply str (doall (for [e (range 16)] (str "<span id='container-" e "'><input id='tick-" e "' type='checkbox'></input></span>")))))
+      (apply str (doall (for [e (range ticks)] (str "<span id='container-" e "'><input id='tick-" e "' type='checkbox'></input></span>")))))
+
+; make a sample array of bonk sound
+(defn make-bonk [p length]
+  (let [sample-count (* mix-loop/sample-rate length)
+        frequency-hz (mix-loop/mtof (+ 60 (mod (* p 5) 23)))]
+    (doall (for [x (range sample-count)]
+             ; envelope linear decay
+             (* (/ (- sample-count x) sample-count)
+                ; sine at the midi pitch specified
+                (js/Math.sin (* (* (/ x mix-loop/sample-rate) frequency-hz) (* js/Math.PI 2))))))))
+
+(defn make-tick-event-fn [e]
+  (fn [ev]
+    (reset! samples
+            (doall (filter #(not (nil? %)) (for [e (range ticks)]
+                                             (if (-> (js/document.getElementById (str "tick-" e)) .-checked)
+                                               {:data (clj->js (make-bonk e 0.1))
+                                                :start-seconds (* e 0.05)
+                                                :length-seconds 0.1})))))))
 
 ; bind events
-
+(doseq [e (range ticks)]
+  (.addEventListener
+    (js/document.getElementById (str "tick-" e))
+    "click"
+    (make-tick-event-fn e)
+    false))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
