0612fb182b6ad92c6eedc981715ff9312c8b46f1
Chris McCormick <chris@mccormick.cx>
2015-12-03 12:09:57 +0800

    Test using a fresh buffer every time - terrible latency issues.

--- src/sir_mix_a_loop/core.cljs
+++ src/sir_mix_a_loop/core.cljs
@@ -3,6 +3,9 @@
 ; check for AudioContext availability and set
 (defonce actx (when (aget js/window "AudioContext") (js/window.AudioContext.)))
 
+; id for each channel
+(defonce channel-id (atom 0))
+
 ; flag for the user to test if audio is available
 (defonce audio? (if actx true false))
 
@@ -27,6 +30,8 @@
     (* (js/Math.log (* f .12231220585)) 17.3123405046)
     -1500))
 
+; run through all samples for the given time and mix the samples there
+; TODO: only examine the samples that actually intersect our range (pre-compute)
 (defn sum-sample-buffers-at-time [samples t-seconds]
   (apply +
          (doall (for [{:keys [data start-seconds length-seconds volume pan sample-sample-rate] :or {volume 1.0 pan 0.0 sample-sample-rate sample-rate}} samples]
@@ -34,34 +39,79 @@
                     (* (aget data (Math.round (* (- t-seconds start-seconds) sample-sample-rate))) volume)
                     0)))))
 
-(defn make-loop-player [length-in-ms]
-  ; HTML5 audio buffer source
-  (let [buffer-source (.createBufferSource actx)
+; update a two channel audio buffer with a loop definition
+; TODO: use clojure.data/diff to only mix the bits of the buffer that changed
+(defn convert-loop-to-buffers [channel old-loop-definition loop-definition]
+  (let [{:keys [length-in-ms samples id]} loop-definition
         frame-count (calculate-frame-count length-in-ms sample-rate)
-        buffer-object (.createBuffer actx 2 frame-count sample-rate)]
-    ; set the loop flag on the buffer source
-    (set! (.-loop buffer-source) true)
-    ; connect it to the audio context output
-    (.connect buffer-source (.-destination actx))
-    {:buffer-source buffer-source
-     :buffer-object buffer-object}))
-
-(defn update-loop [loop-player loop-definition]
-  (let [{:keys [length-in-ms samples]} loop-definition
-        {:keys [buffer-source buffer-object]} loop-player
-        frame-count (calculate-frame-count length-in-ms sample-rate)]
+        buffers (or (channel :buffers) (.createBuffer actx 2 frame-count sample-rate))]
     ; for left and right channels
     (doseq [c (range 2)]
       ; grab the raw js audio buffer for this channel to write to
-      (let [buffer (.getChannelData buffer-object c)]
+      (let [buffer (.getChannelData buffers c)]
         ; for each individual sample in the count
         (doseq [s (range frame-count)]
           (let [t-seconds (/ s sample-rate)]
             ; mutate the audio buffer to add this sample value
             (aset buffer s
-                  (sum-sample-buffers-at-time samples t-seconds))))))
-    ; force browsers to refresh the buffer
-    (try
-      (set! (.-buffer buffer-source) buffer-object)
-      ; catch this on Chrome: "Uncaught TypeError: Failed to set the 'buffer' property on 'AudioBufferSourceNode'" w@ it works anyway
-      (catch :default e e))))
+                  (sum-sample-buffers-at-time samples t-seconds))))
+        (js/console.log buffer)
+        ))
+    buffers))
+
+(defn make-channel-player [new-buffers]
+  ; HTML5 audio buffer source
+  (let [buffer-source (.createBufferSource actx)
+        frame-count (.-length new-buffers)
+        buffer-object (.createBuffer actx 2 frame-count sample-rate)]
+    ; copy the data from one buffer to the other
+    ; for left and right channels
+    (doseq [c (range 2)]
+      ; grab the raw js audio buffer for this channel to write to
+      (let [buffer-dest (.getChannelData buffer-object c)
+            buffer-source (.getChannelData new-buffers c)]
+        ; for each individual sample in the count
+        (doseq [s (range frame-count)]
+          ; mutate the audio buffer to add this sample value
+          (aset buffer-dest s (aget buffer-source s)))))
+    ; set the buffer source on our new objects
+    (set! (.-buffer buffer-source) buffer-object)
+    ; set the loop flag on the buffer source
+    (set! (.-loop buffer-source) true)
+    ; connect it to the audio context output
+    (.connect buffer-source (.-destination actx))
+    ; pass back the thing that is playable
+    buffer-source))
+
+(defn make-channel
+  "Create a new channel that can be used to play a loop of audio."
+  []
+  {:start-time nil :id (swap! channel-id inc) :buffers nil :player nil :loop-definition nil})
+
+(defn update-channel-data
+  "Update the audio data that a channel should play."
+  [channel new-loop-definition]
+  (let [{:keys [start-time id buffers player loop-definition] :as channel} channel
+        ; create / update the audio buffers with the new data
+        new-buffers (convert-loop-to-buffers channel loop-definition new-loop-definition)
+        ; make a new player object
+        new-player (make-channel-player new-buffers)
+        ; update the channel definition with our changes
+        modified-channel (assoc channel
+                                :buffers new-buffers
+                                :player new-player
+                                :loop-definition new-loop-definition)]
+    ; if the channel is already playing, continue playback at the correct sample
+    (when start-time
+      (.stop (channel :player))
+      (.start (modified-channel :player (mod (- (get-audio-time) (channel :start-time)) (new-loop-definition :length-in-ms)))))
+    modified-channel))
+
+(defn play-channel
+  "Start a channel playing - returns the modified channel."
+  [channel]
+  (let [now (get-audio-time)]
+    (if (channel :player)
+      (.start (channel :player))
+      (print (str "Channel " (channel :id) " has no data yet!")))
+    (assoc channel :start-time now)))
--- src/sir_mix_a_loop/dev.cljs
+++ src/sir_mix_a_loop/dev.cljs
@@ -5,62 +5,71 @@
 
 (println "Sir Mix-a-loop dev mode.")
 
-; (defonce app-state (atom {:text "Hello world!"}))
-
 (defonce samples (atom []))
-
 (def ticks 16)
-
 (def my-loop {:length-in-ms 800})
 
-(defonce looper
-  (if mix-loop/audio?
-    (let [looper (mix-loop/make-loop-player (:length-in-ms my-loop))]
-      (.start (looper :buffer-source)) 
-      looper)
-    (js/alert "Audio not available in your browser, sorry!")))
+; get our channel to play a loop on
+(defonce channel
+  (atom
+    (if mix-loop/audio?
+      (mix-loop/make-channel)
+      (js/alert "Audio not available in your browser, sorry!"))))
 
-(when looper
+; if we have audio available
+; mix the first loop
+(when @channel
   (print "Mixing.")
-  (mix-loop/update-loop looper my-loop))
+  (swap! channel mix-loop/update-channel-data my-loop)
+  (swap! channel mix-loop/play-channel))
 
+; *** LOOP REGENERATE *** ;
+
+; make a sample array of bonk sound
+(defn make-bonk [p length]
+  (let [sample-count (* mix-loop/sample-rate length)
+        frequency-hz (mix-loop/mtof (+ 60 (mod (* p 5) 23)))
+        result (doall (for [x (range sample-count)]
+                        ; envelope linear decay
+                        (* 0.5 (/ (- (- sample-count 1) x) sample-count)
+                           ; sine at the midi pitch specified
+                           (js/Math.sin (* (* (/ x mix-loop/sample-rate) frequency-hz) (* js/Math.PI 2))))))]
+    (print (last result))
+    (print sample-count)
+    result))
+
+; function that gets run when the loop is changed
 (defn samples-watcher
   [watcher-key atom-changed old-state new-state]
-  (mix-loop/update-loop looper (assoc my-loop :samples @samples)))
+  (swap! channel mix-loop/update-channel-data
+         (assoc my-loop :samples @samples)))
 
 ; when the samples atom is changed, update the loop
 (defonce watcher
   (add-watch samples :loop-updater samples-watcher))
 
+; *** UI **** ;
+
 ; create our user interface
 (set! (.-innerHTML (js/document.getElementById "app"))
       (apply str (doall (for [e (range ticks)] (str "<span id='container-" e "'><input id='tick-" e "' type='checkbox'></input></span>")))))
 
-; make a sample array of bonk sound
-(defn make-bonk [p length]
-  (let [sample-count (* mix-loop/sample-rate length)
-        frequency-hz (mix-loop/mtof (+ 60 (mod (* p 5) 23)))]
-    (doall (for [x (range sample-count)]
-             ; envelope linear decay
-             (* (/ (- sample-count x) sample-count)
-                ; sine at the midi pitch specified
-                (js/Math.sin (* (* (/ x mix-loop/sample-rate) frequency-hz) (* js/Math.PI 2))))))))
-
-(defn make-tick-event-fn [e]
+; what happens when one of the checkboxes is clicked
+(defn make-checkbox-event-fn [e]
   (fn [ev]
     (reset! samples
             (doall (filter #(not (nil? %)) (for [e (range ticks)]
                                              (if (-> (js/document.getElementById (str "tick-" e)) .-checked)
-                                               {:data (clj->js (make-bonk e 0.1))
+                                               {:data (clj->js (make-bonk e 0.5))
                                                 :start-seconds (* e 0.05)
-                                                :length-seconds 0.1})))))))
+                                                :length-seconds 0.5})))))))
 
-; bind events
+; bind events to the page
 (doseq [e (range ticks)]
   (.addEventListener
     (js/document.getElementById (str "tick-" e))
     "click"
-    (make-tick-event-fn e)
+    (make-checkbox-event-fn e)
     false))
 
 (defn on-js-reload []
